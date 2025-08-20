5855d60e3e28b66a9f406bf085e2b8e0210eaea4
Jawad Kotaich <155095794+JawadKotaichh@users.noreply.github.com>
2025-08-20 20:30:12 +0300

    finally adjusted :)

--- tempo_aware/generate_audio_from_tokens.py
+++ tempo_aware/generate_audio_from_tokens.py
@@ -2,14 +2,16 @@ import soundfile as sf
 import numpy as np
 import re
 import os
-from get_corpus import get_note_duration
+from get_corpus import get_note_duration, get_corpus
 
-def generate_audio_from_tokens(list_of_tokens, tempo, audio_folder="../data/fundemental_hits"):
 
+def generate_audio_from_tokens(
+    list_of_tokens, tempo, audio_folder="../data/fundemental_hits"
+):
     quarter_duration = 60.0 / tempo[0]
     note_durations = get_note_duration(quarter_duration)
-    
-    token_re = re.compile(r'([A-Za-z])_(\d+(?:\.\d+)?)')
+
+    token_re = re.compile(r"([A-Za-z])_(\d+(?:\.\d+)?)")
 
     chosen_paths = []
     durations = []
@@ -20,7 +22,7 @@ def generate_audio_from_tokens(list_of_tokens, tempo, audio_folder="../data/fund
             dur_key = float(dur_str)
             dur_seconds = note_durations[dur_key]
             wav_file = os.path.join(audio_folder, f"{letter}.wav")
-            
+
             chosen_paths.append(wav_file)
             durations.append(dur_seconds)
             print(f"→ will play {letter}.wav for {dur_seconds:.3f}s")
@@ -32,7 +34,7 @@ def generate_audio_from_tokens(list_of_tokens, tempo, audio_folder="../data/fund
             data = np.mean(data, axis=1)
         # data = data * 3
 
-        target_len = int(dur * fs) #in samples
+        target_len = int(dur * fs)  # in samples
         if data.shape[0] < target_len:
             pad = np.zeros(target_len - data.shape[0], dtype=data.dtype)
             data = np.concatenate([data, pad])
@@ -41,4 +43,10 @@ def generate_audio_from_tokens(list_of_tokens, tempo, audio_folder="../data/fund
         segments.append(data)
 
     combined = np.concatenate(segments)
-    sf.write("output.wav", data=combined, samplerate=fs)
+    sf.write("old1_regen.wav", data=combined, samplerate=fs)
+
+
+list_of_tokens, tempo = get_corpus(
+    fundamentals_path="../mel_48000.json", file_path="../data/first_data/old1.wav"
+)
+generate_audio_from_tokens(list_of_tokens=list_of_tokens, tempo=tempo)
--- tempo_aware/get_corpus.py
+++ tempo_aware/get_corpus.py
@@ -10,14 +10,14 @@ def get_note_duration(quarter_duration):
         4: quarter_duration,
         2: quarter_duration * 2,
         1: quarter_duration * 4,
-        24: quarter_duration/6,
-        32:quarter_duration/8,
-        12:quarter_duration/3,
-        2.67:quarter_duration*(3/2),
-        5.34:quarter_duration*(3/4),
-        10.67:quarter_duration*(3/8),
-        21.33:quarter_duration*(3/16),
-        1.33:quarter_duration*(3)
+        24: quarter_duration / 6,
+        32: quarter_duration / 8,
+        12: quarter_duration / 3,
+        2.67: quarter_duration * (3 / 2),
+        5.34: quarter_duration * (3 / 4),
+        10.67: quarter_duration * (3 / 8),
+        21.33: quarter_duration * (3 / 16),
+        1.33: quarter_duration * (3),
         # 28:quarter_duration*(1/7),
         # 56:quarter_duration*(1/14),
         # 14:quarter_duration*(2/7),
@@ -49,14 +49,16 @@ def sliding_cross_correlation(X, Y):
     _, n_time_Y = Y.shape
 
     best_score = -np.inf
-    best_offset = None 
+    best_offset = None
     norm_X = np.linalg.norm(X)
     for offset in range(n_time_Y - n_time_X + 1):
-        
-        Y_slice = Y[:, offset:offset + n_time_X]
-        if (norm_X!=0 and np.linalg.norm(Y_slice)!=0) :
-            score = np.tensordot(X, Y_slice, axes=2) / (norm_X * np.linalg.norm(Y_slice)) 
-        else: continue
+        Y_slice = Y[:, offset : offset + n_time_X]
+        if norm_X != 0 and np.linalg.norm(Y_slice) != 0:
+            score = np.tensordot(X, Y_slice, axes=2) / (
+                norm_X * np.linalg.norm(Y_slice)
+            )
+        else:
+            continue
 
         if score > best_score:
             best_score = score
@@ -66,68 +68,123 @@ def sliding_cross_correlation(X, Y):
 
 
 def load_json(file_path):
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         mels = json.load(f)
-    return {name:np.array(mel) for name,mel in mels.items()}
+    return {name: np.array(mel) for name, mel in mels.items()}
 
 
 def load_file(file_path):
-    y ,sr =librosa.load(file_path, sr=None)
-    y=librosa.util.normalize(y)
+    y, sr = librosa.load(file_path, sr=None)
+    y = librosa.util.normalize(y)
     return y, sr
 
 
-def get_onsets(y,sr):
-    onsets=librosa.onset.onset_detect(y=y , sr=sr)
-    onsets_avg=[onsets[0]//2]
-    for i in range(len(onsets)-1):
-        onsets_avg.append((onsets[i]+onsets[i+1])//2)
-    onsets_avg=librosa.frames_to_samples(onsets_avg)
-    return onsets_avg
-
-
 def adjust_tempo(y, sr):
-    tempo,_=librosa.beat.beat_track(y=y,sr=sr)
-    while tempo>150:
-        tempo=tempo//2
-    while tempo<50:
-        tempo*=2
+    tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
+    # while tempo > 150:
+    #     tempo = tempo // 2
+    # while tempo < 50:
+    #     tempo *= 2
+    print(f"tempo : {tempo}")
     return tempo
 
 
-def get_intervals(y , sr):
-    onsets=get_onsets(y,sr)
-    intervals=[]
-    for i in range(len(onsets)-1):
-        intervals.append((onsets[i],onsets[i+1]))
+def get_onsets(y, sr, hop_length=512):
+    onset_frames = librosa.onset.onset_detect(y=y, sr=sr, hop_length=hop_length)
+
+    # Handle edge cases (0 or 1 onset)
+    if len(onset_frames) == 0:
+        # one big region: start=0, end=n_frames
+        n_frames = 1 + (len(y) // hop_length)
+        return librosa.frames_to_samples([0, n_frames], hop_length=hop_length)
+
+    if len(onset_frames) == 1:
+        n_frames = 1 + (len(y) // hop_length)
+        return librosa.frames_to_samples(
+            [0, onset_frames[0], n_frames], hop_length=hop_length
+        )
+
+    # Midpoints between consecutive onsets (still in frames)
+    mids = [
+        (onset_frames[i] + onset_frames[i + 1]) // 2
+        for i in range(len(onset_frames) - 1)
+    ]
+    n_frames = int(np.ceil(len(y) / hop_length))
+    boundaries_frames = [0] + mids + [n_frames]
+    return librosa.frames_to_samples(boundaries_frames, hop_length=hop_length)
+
+
+# def get_onsets(y, sr):
+#     onsets = librosa.onset.onset_detect(y=y, sr=sr)
+#     print("onsets: ", len(onsets))
+#     onsets_avg = [onsets[0] // 2]
+#     for i in range(len(onsets) - 1):
+#         onsets_avg.append((onsets[i] + onsets[i + 1]) // 2)
+#     onsets_avg.append((librosa.samples_to_frames([len(y)])[0] - onsets[-1]) // 2)
+#     onsets_avg = librosa.frames_to_samples(onsets_avg)
+#     return onsets_avg
+
+
+# def get_intervals(y, sr):
+#     onsets = get_onsets(y, sr)
+#     print("len(onsets): ", len(onsets))
+#     intervals = []
+#     for i in range(len(onsets) - 1):
+#         intervals.append((onsets[i], onsets[i + 1]))
+#     return intervals
+
+
+def get_intervals(y, sr, hop_length=512):
+    boundaries = get_onsets(y, sr, hop_length=hop_length)  # in samples
+    intervals = [(boundaries[i], boundaries[i + 1]) for i in range(len(boundaries) - 1)]
+    return intervals
+
+
+def get_intervals_for_duration(y, sr, hop_length=512):
+    boundaries = librosa.onset.onset_detect(
+        y=y, sr=sr, hop_length=hop_length, units="samples"
+    )
+    intervals = [(boundaries[i], boundaries[i + 1]) for i in range(len(boundaries) - 1)]
+    intervals.append((boundaries[-1], len(y)))
+    print("onset_duration intervals", len(intervals))
     return intervals
 
 
 def get_corpus(fundamentals_path, file_path):
-    y,sr=load_file(file_path)
-    fundamentals=load_json(fundamentals_path)
-    intervals=get_intervals(y,sr)
-    tempo=adjust_tempo(y,sr)
-    quarter_duration=60.0/tempo # in seconds
-    note_durations=get_note_duration(quarter_duration) # in seconds
-    classified_hits=[]
-    for interval in intervals:
-        segment=y[interval[0]:interval[1]]
-        mel=librosa.feature.melspectrogram(y=segment, sr=sr) # TODO revisit
-        best_score=-np.inf
-        best_hit=''
+    y, sr = load_file(file_path)
+    y = librosa.resample(y, orig_sr=sr, target_sr=48000)
+    sr = 48000
+    y = np.concatenate([np.zeros(48000), y])
+    fundamentals = load_json(fundamentals_path)
+    intervals_duration = get_intervals_for_duration(y, sr)
+    intervals = get_intervals(y, sr)
+
+    tempo = adjust_tempo(y, sr)
+    quarter_duration = 60.0 / tempo  # in seconds
+    note_durations = get_note_duration(quarter_duration)  # in seconds
+    classified_hits = []
+    for interval, interval_dur in zip(intervals, intervals_duration):
+        segment = y[interval[0] : interval[1]]
+        mel = librosa.feature.melspectrogram(y=segment, sr=sr)  # TODO revisit
+        best_score = -np.inf
+        best_hit = ""
         for fundamental_hit in fundamentals:
-            score,_=sliding_cross_correlation(mel,fundamentals[fundamental_hit])
-            if score>best_score:
-                best_score=score
-                best_hit=fundamental_hit
-        hit_duration=len(segment)/sr # in seconds
-        min_diff=np.inf
-        best_note=''
+            score, _ = sliding_cross_correlation(mel, fundamentals[fundamental_hit])
+            if score > best_score:
+                best_score = score
+                best_hit = fundamental_hit
+        hit_duration = (interval_dur[1] - interval_dur[0]) / sr  # in seconds
+        min_diff = np.inf
+        best_note = ""
         for note in note_durations:
-            diff=abs(note_durations[note]-hit_duration)
-            if diff<min_diff:
-                min_diff=diff
-                best_note=str(note)
-        classified_hits.append(best_hit+'_'+best_note)
+            diff = abs(note_durations[note] - hit_duration)
+            if diff < min_diff:
+                min_diff = diff
+                best_note = str(note)
+        classified_hits.append(best_hit + "_" + best_note)
     return classified_hits, tempo
+
+
+# classified_hits, _ = get_corpus(
+#     fundamentals_path="../mel_48000.json", file_path="../data/first_data/old1.wav"
+# )
