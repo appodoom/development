134fc4c06d175aea1efbb76b3db15a8526403d0f
Jawad Kotaich <jawadkotaich@gmail.com>
2025-11-19 12:17:18 +0200

    t

--- CNN_model/t.py
+++
@@ -1,9 +0,0 @@
-import os
-import glob
-
-DATA_DIR = "../data/pairs"
-
-print("DATA_DIR exists:", os.path.isdir(DATA_DIR))
-files = glob.glob(os.path.join(DATA_DIR, "*.wav"))
-print("Number of .wav files:", len(files))
-print("First few files:", files[:5])
