527500ba544c2c47e8a0d4f2a801feab43fa1fce
Ali Slim <ass55@mail.aub.edu>
2025-12-21 21:27:33 +0200

    derbukka to input done

--- derbukka_to_input.py
+++ derbukka_to_input.py
@@ -1,6 +1,5 @@
 """This script takes the derbukka file format and turns it into a 
-sequence of hits where each hit is represented by 
-(hit_type, hit_subd, delta_tempo, amplitude, deviation)"""
+sequence of tokens thatt will be taken as input by the model"""
 
 import json
 
@@ -21,39 +20,38 @@ def get_arrays(content):
     return global_tempo, tempos, skeleton, variations
 
 def get_sequence(file_path):
-    sequence=[]
     data=load_file(file_path=file_path)
     global_tempo, tempos, skeleton, variations= get_arrays(content=data)
+    sequence=["TEMPO_"+str(tempos[0]-global_tempo)]
     i=0
     j=0
     k=0
     count=0
     value=0
+    previous_tempo=global_tempo
     while (i!=len(skeleton)):
         if skeleton[i].split("_")[0]=="DELAY":
             count=int(skeleton[i].split("_")[1])
             i+=1
         else:
-            sequence.append((skeleton[i],"SUBD_0",tempos[k]-global_tempo,"AMP_1.5",skeleton[i+1]))
+            sequence.append(skeleton[i])
+            sequence.append(skeleton[i+1])
             i+=2
         while (count!=0 and j!=len(variations)):
             if variations[j].split("_")[0]=="SUBD":
                 value=int(variations[j].split("_")[1])
+                sequence.append(variations[j])
                 j+=1
             else:
-                sequence.append((variations[j],"SUBD_"+str(value), tempos[k]-global_tempo, variations[j+1],"DEV_0"))
+                sequence.append(variations[j])
+                sequence.append(variations[j+1])
                 j+=2
                 count-=1/value
                 if count%1==0:
+                    previous_tempo=tempos[k]
                     k+=1
-    sequence_skeleton=[]
-    sequence_variations=[]
-    for hit in sequence:
-        if hit[1]=="SUBD_0":
-            sequence_skeleton.append(hit)
-        else:
-            sequence_variations.append(hit)
-    sequences={"skeleton":sequence_skeleton, "variations": sequence_variations, "sequence":sequence, "global_tempo":global_tempo}
+                    sequence.append("TEMPO_"+str(tempos[k]-previous_tempo))
+    sequences={"sequence":sequence}
     save_json(data=sequences, file_name="sequence.json")
 get_sequence("sample.txt")
 
---
+++ output_to_derbukka.py
@@ -0,0 +1,46 @@
+"""This script takes the ouput of the model 
+and turns it into a derbukka file"""
+
+import json
+def load_json(directory):
+    with open(directory, 'r') as file:
+        data = json.load(file)
+    return data["sequence"]
+
+def write_derbukka(file_name,string):
+    with open(file_name, "w") as file:
+        file.write(string)
+
+def to_derbukka(directory,initial_tempo):
+    sequence=load_json(directory)
+    initial_tempo_str=str(initial_tempo)
+    count=0
+    value=0
+    tempos_str=""
+    skeleton_str=""
+    variations_str=""
+    for i in range(len(sequence)):
+        if sequence[i].split("_")[0]=="TEMPO":
+            tempos_str+=str(initial_tempo+float(sequence[i].split("_")[1]))
+            tempos_str+=" "
+        elif sequence[i].split("_")[0]=="SUBD":
+            variations_str+=sequence[i]
+            variations_str+=" "
+            value=1/int(sequence[i].split("_")[1])
+        elif sequence[i].split("_")[0]=="AMP":
+            variations_str+=sequence[i-1]+" "+sequence[i]+" "
+        elif sequence[i].split("_")[0]=="DEV":
+            skeleton_str+="DELAY_"+str(count)+" "+sequence[i-1]+" "+sequence[i]+" "
+            count=0
+        else:
+            continue
+        count+=value
+    write_derbukka("output.txt", initial_tempo_str+"\n"+tempos_str+"\n"+skeleton_str+"\n"+variations_str)
+
+to_derbukka("sequence.json", 110.0)
+
+
+
+
+
+
